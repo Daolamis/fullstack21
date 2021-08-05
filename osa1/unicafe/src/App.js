347d3f96b98d1caf92cb25f3d509f5885e72692a
Petteri Parviainen <daolamis@gmail.com>
2021-08-05 12:28:30 +0300

    implemented exercise 1.7

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const all = good + neutral + bad;
+  const value = good - bad;
+  const avarage = value / all || 0;
+  const positive = good / all * 100 || 0;
 
   return (
     <div>
@@ -12,11 +16,12 @@ const App = () => {
       <button onClick={() => setGood(good + 1)} >good</button>
       <button onClick={() => setNeutral(neutral + 1)} >neutral</button>
       <button onClick={() => setBad(bad + 1)} >bad</button>
-      <p>
-        good {good} <br />
-        neutral {neutral} <br />
-        bad {bad}  <br />
-      </p>
+      <h2>statistics</h2>
+      good {good} <br />
+      neutral {neutral} <br />
+      bad {bad}  <br />
+      avarage {avarage} <br />
+      positive {positive} %
     </div>
   )
 }
