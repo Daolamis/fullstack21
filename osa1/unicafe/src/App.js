653deca1bf3191c6212d6209992a3870c91f3b16
Petteri Parviainen <daolamis@gmail.com>
2021-08-05 12:56:13 +0300

    implemented exercise 1.11

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -8,15 +8,15 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text='good'/>
-      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral'/>
-      <Button handleClick={() => setBad(bad + 1)} text='bad'/>
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
 
-const Button = ({text, handleClick}) => (<button onClick={() => handleClick()} >{text}</button>)
+const Button = ({ text, handleClick }) => (<button onClick={() => handleClick()} >{text}</button>)
 
 const Statistics = ({ good, neutral, bad }) => {
   if (!good && !neutral && !bad) {
@@ -29,14 +29,18 @@ const Statistics = ({ good, neutral, bad }) => {
   const positive = good / all * 100 || 0;
   return <div>
     <h2>statistics</h2>
-    <StatisticsLine text='good' value={good}/>
-    <StatisticsLine text='neutral' value={neutral}/>
-    <StatisticsLine text='bad' value={bad}/>
-    <StatisticsLine text='avarage' value={avarage}/>
-    <StatisticsLine text='positive' value={positive}/>
+    <table>
+      <tbody>
+        <StatisticsLine text='good' value={good} />
+        <StatisticsLine text='neutral' value={neutral} />
+        <StatisticsLine text='bad' value={bad} />
+        <StatisticsLine text='avarage' value={avarage} />
+        <StatisticsLine text='positive' value={positive+' %'} />
+      </tbody>
+    </table>
   </div>
 }
 
-const StatisticsLine = ({text, value}) => <div>{text} {value}</div>
+const StatisticsLine = ({ text, value }) => <tr><td>{text}</td><td>{value}</td></tr>
 
 export default App
