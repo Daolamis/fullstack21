9fc0f0bafe1f9086eaa9a104d48ebf00bafafcb1
Petteri Parviainen <daolamis@gmail.com>
2021-08-05 12:32:53 +0300

    implementes exercise 1.8

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -5,25 +5,31 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const all = good + neutral + bad;
-  const value = good - bad;
-  const avarage = value / all || 0;
-  const positive = good / all * 100 || 0;
-
   return (
     <div>
       <h1>Give feedback</h1>
       <button onClick={() => setGood(good + 1)} >good</button>
       <button onClick={() => setNeutral(neutral + 1)} >neutral</button>
       <button onClick={() => setBad(bad + 1)} >bad</button>
-      <h2>statistics</h2>
-      good {good} <br />
-      neutral {neutral} <br />
-      bad {bad}  <br />
-      avarage {avarage} <br />
-      positive {positive} %
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const value = good - bad;
+  const avarage = value / all || 0;
+  const positive = good/all * 100 || 0;
+  return <div>
+    <h2>statistics</h2>
+    good {good} <br />
+    neutral {neutral} <br />
+    bad {bad} <br />
+    all {all}<br />
+    avarage {avarage} <br />
+    positive {positive} %
+  </div>
 }
 
 export default App
