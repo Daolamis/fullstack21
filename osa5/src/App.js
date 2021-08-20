b3daf9c503012f83174599a4ce0e46e859b68e94
Petteri Parviainen <daolamis@gmail.com>
2021-08-20 10:11:47 +0300

    implemented exercise 5.1

--- osa5/src/App.js
+++ osa5/src/App.js
@@ -1,22 +1,55 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const user = await loginService.login({ username, password });
+
+    setUser(user);
+    setUsername('');
+    setPassword('');
+  }
+
+  const loginForm = () => (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>Username: <input type='text' value={username} onChange={e => setUsername(e.target.value)} /></div>
+        <div>Password: <input type='password' value={password} onChange={e => setPassword(e.target.value)} /></div>
+        <div><button type='submit'>login</button></div>
+      </form>
+    </div>)
+
+  const blogList = () => (
+    <div>
+      <h2>blogs</h2>
+      <p>{`${user.name} logged in`}</p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user === null ?
+        loginForm() :
+        blogList()
+      }
     </div>
   )
 }
