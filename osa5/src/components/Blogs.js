eef3efd3233c22374127ba44f512fca6844fcb91
Petteri Parviainen <daolamis@gmail.com>
2021-08-20 16:13:54 +0300

    implemented exercise 5.7

--- osa5/src/components/Blogs.js
+++ osa5/src/components/Blogs.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Blogs = ({ blogs }) => (
   blogs.map(blog =>
     <Blog key={blog.id} blog={blog} />
   )
 )
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false);
+  const toggleView = () => setView(!view);
+
+  const buttonLabel = view ? 'hide' : 'view'
+  return (
+    <div className='blog'>
+      {blog.title} {blog.author} <button onClick={toggleView}>{buttonLabel}</button>
+      <div style={{ display: view ? '' : 'none' }} >
+        <div>{blog.url}</div>
+        <div>{blog.likes} <button>like</button></div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blogs
