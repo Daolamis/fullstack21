ff7db90fece8c070987eb97ccbc28e63c2e74f47
Petteri Parviainen <daolamis@gmail.com>
2021-08-26 10:04:55 +0300

    implemented exercise 6.11

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { addVote } from '../reducers/anecdoteReducer'
+import { showNotification, hideNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
   const anecdotes = useSelector(state => state.anecdotes)
   const dispatch = useDispatch()
+  let notificationTimeout = useRef()
 
-  const vote = (id) => {
-    dispatch(addVote(id));
+  const vote = (anecdote) => {
+    clearTimeout(notificationTimeout.current)
+    dispatch(addVote(anecdote.id));
+    dispatch(showNotification(`you voted '${anecdote.content}'`))
+    notificationTimeout.current = setTimeout(() => dispatch(hideNotification()), 5000)
   }
 
   return anecdotes.map(anecdote =>
@@ -17,7 +22,7 @@ const AnecdoteList = () => {
       </div>
       <div>
         has {anecdote.votes}
-        <button onClick={() => vote(anecdote.id)}>vote</button>
+        <button onClick={() => vote(anecdote)}>vote</button>
       </div>
     </div>
   )
--- osa6/redux-anecdotes/src/reducers/notificationReducer.js
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,5 +1,25 @@
-const reducer = (state='Some note', action) => {
-  return state
+const reducer = (state = null, action) => {
+  switch (action.type) {
+    case 'SHOW':
+      return action.notification
+    case 'HIDE':
+      return null;
+    default:
+      return state;
+  }
+}
+
+export const showNotification = (notification) => {
+  return {
+    type: 'SHOW',
+    notification
+  }
+}
+
+export const hideNotification = () => {
+  return {
+    type: 'HIDE',
+  }
 }
 
 export default reducer;
